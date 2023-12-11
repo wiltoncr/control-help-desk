257fea5c36a76fe31dcef752ded47c15f593ae3c
wilton cr <wltonmeg4@gmail.com>
2023-12-11 16:44:26 -0300

    add eslint and editorconfig

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const { app } = require('./app.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT  ;
 
 app.listen(PORT, () => {
     console.log("start server or port " + PORT)
