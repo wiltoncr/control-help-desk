e28802e313b8b2a42dfe0b217ab38760e6ec0088
Wiltoncr <wiltonmeg4@gmail.com>
2024-07-05 18:00:27 -0300

    add cors in app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { router } = require('./router');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
